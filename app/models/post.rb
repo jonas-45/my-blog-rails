fcd5de13f9105462e36ea77dd29dc2930d86a0b4
jonas-45 <jonaswullah@gmail.com>
2023-05-20 12:45:03 +0000

    Make updat_posts_counter method private

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,13 +9,13 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0}
   validates :likes_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
-  def update_posts_counter
-    author.update(posts_counter: author.posts_counter + 1)
-  end
-
   def five_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
-  private :update_posts_counter
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
 end

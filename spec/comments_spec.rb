f3b3a3ded22b0984a95e9bbc49af0b5e19a729ca
jonas-45 <jonaswullah@gmail.com>
2023-05-20 14:40:49 +0000

    Resolve linter errors

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Comment, type: :model do
   let(:user) { User.create(name: 'Jake', photo: 'user.png', bio: 'Jake is a 21-year-old footballer from Ghana') }
-  let(:post) { Post.create(author: user, title: 'My first post', text: 'Ghana is in West Africa') }
+  let(:post1) { Post.create(author: user, title: 'My first post', text: 'Ghana is in West Africa') }
 
   describe 'Associations' do
     it 'belongs to an author' do
@@ -21,11 +21,11 @@ describe Comment, type: :model do
   describe 'Callbacks' do
     describe 'after_save' do
       it 'updates the comments_counter of the associated post' do
-        expect(post.comments_counter).to eq(0)
+        expect(post1.comments_counter).to eq(0)
 
-        comment = Comment.create(author: user, post: post, text: 'First comment')
+        Comment.create(author: user, post: post1, text: 'First comment')
 
-        expect(post.reload.comments_counter).to eq(1)
+        expect(post1.reload.comments_counter).to eq(1)
       end
     end
   end

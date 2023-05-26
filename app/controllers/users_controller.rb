43455403a7bcac13218330753873aad566d75706
jonas-45 <jonaswullah@gmail.com>
2023-05-26 19:00:42 +0000

    Add fetching users with their post to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
     @all_users = User.all
   end
 
-  def show; end
+  def show 
+    @user = User.find(params[:id])
+    # @user_posts = Post.where(author_id: @user.id)
+    @recent_posts = @user.three_recent_posts
+  end
 end

616f5a542eb826f32980b279b5d575e7db9f57c2
jonas-45 <jonaswullah@gmail.com>
2023-05-26 19:18:36 +0000

    Resolve linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @all_users = User.all
   end
 
-  def show 
+  def show
     @user = User.find(params[:id])
     # @user_posts = Post.where(author_id: @user.id)
     @recent_posts = @user.three_recent_posts

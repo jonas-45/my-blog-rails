77053e076220e277a7737526f76c0d1a9925098a
Habtamu210 <beleyaha@gmail.com>
2023-06-03 10:13:23 -0700

    Fix the errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,7 @@ class ApplicationController < ActionController::Base
   end
 end
 
+
 protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?

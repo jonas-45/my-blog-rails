bbe92ae3054896c3d4fc4572d1b58891f6bae84a
jonas-45 <jonaswullah@gmail.com>
2023-06-03 20:49:47 +0000

    Add after_sign_up_path_for method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
-  
-end
-
-
-protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -17,3 +13,8 @@ protect_from_forgery with: :exception
   def after_sign_in_path_for(_resource)
     users_path
   end
+
+  def after_sign_up_path_for(_resource)
+    users_path
+  end
+end
